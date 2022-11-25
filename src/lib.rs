9d58cb79a64c4b11fec74c1871d6422864cdfc75
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-25 01:39:47 +0000

    Refactor to lib + bin for easier testing

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub async fn run() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health_check))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
