01209980c56e132b12b385487a91505e17f552f1
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-26 08:03:45 +0000

    Reorganize code to make it easier to add features

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, HttpResponse, Responder};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    pub email: String,
+    pub name: String
+}
+
+/// Extract form data using serde.
+/// This handler get called only if content type is *x-www-form-urlencoded*
+/// and content of the request could be deserialized to a `FormData` struct
+pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
