fd0de05f5cade812e9215a64c375aa0e7f77a2f5
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-11-26 18:23:34 +0000

    Add database setup for local and CI, and use it in a test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 //! tests/health_check.rs
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
+
 #[cfg(test)]
 use pretty_assertions::assert_eq; // can also add assert_ne if needed
 
@@ -53,6 +56,11 @@ async fn health_check_alias_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let base_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     // Act
@@ -64,8 +72,17 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .send()
         .await
         .expect("Failed to execute request.");
-// Assert
-assert_eq!(200, response.status().as_u16());
+    
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch a subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 
